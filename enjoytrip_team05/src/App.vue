156393e7075c58189dca94362a6d88b3b641f1e7
고관우 <kwko97@gmail.com>
2024-05-10 17:59:05 +0900

    feat: navbar

--- enjoytrip_team05/src/App.vue
+++ enjoytrip_team05/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 
+import TheHeadingNavbar from "@/views/TheHeadingNavbar.vue";
+import TheMainView from "@/views/TheMainView.vue";
 </script>
 
 <template>
   <div>
-
+    <TheHeadingNavbar />
+    <TheMainView />
   </div>
 </template>
 
