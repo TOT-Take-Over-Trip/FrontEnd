bad98e767708c7078c8784992e8b3b694e5600e6
고관우 <kwko97@hanyang.ac.kr>
2024-05-14 15:43:20 +0900

    feat: 게시판 UI 완성, 데이터 처리 필요

--- enjoytrip_team05/src/components/board/BoardSearchComponent.vue
+++ enjoytrip_team05/src/components/board/BoardSearchComponent.vue
@@ -1,23 +1,23 @@
 <script setup>
-
 import VButton from "@/components/VButton.vue";
+
 </script>
 
 <template>
-  <div class="flex justify-around items-center w-full border-b-2">
+  <div class="flex justify-around items-center w-full ">
     <!--  검색 + 검색아이콘 + 필터 + 글쓰기 -->
-    <div class="flex items-center w-2/3 h-32">
+    <div class="flex items-center justify-center w-2/3 h-24">
       <!--  TODO: select 아예 component로 사용할 수 있는 거 만들기    -->
       <!--  TODO: 이거 필터 내용 어떤거 할건지?    -->
-      <select class="me-12" style="height: 50%">
+      <select class="me-12" style="height: 30%; width: 8%">
         <option>제목</option>
         <option>작성자</option>
       </select>
-      <input type="text" class="text-2xl" style="width: 50%; height: 50%" placeholder="검색할 내용을 입력해주세요.">
+      <input type="text" class="text-xl border-2" style="width: 50%; height: 45%" placeholder="검색할 내용을 입력해주세요.">
       <!--  TODO: click 할 때 게시글 검색으로 넘어가야돼    -->
       <img src="/src/assets/img/searchIcon.png" @click="" class="mx-3" style="height: 49%" alt="searchIcon" />
+      <VButton class="w-32" style="height: 40%;">글쓰기</VButton>
     </div>
-    <VButton class="w-32 h-3/4">글쓰기</VButton>
   </div>
 </template>
 
--- enjoytrip_team05/src/views/board/TheBoardView.vue
+++ enjoytrip_team05/src/views/board/TheBoardView.vue
@@ -1,66 +1,88 @@
 <script setup>
+import axios from 'axios';
+import {onMounted, ref} from "vue";
 import { Swiper, SwiperSlide } from 'swiper/vue';
+
 
 // Import Swiper styles
 import 'swiper/css';
 import VBoardCard from "@/components/board/BoardCard.vue";
 import SearchComponent from "@/components/board/BoardSearchComponent.vue";
+import BoardContent from "@/components/board/BoardContent.vue";
 
-const onSwiper = (swiper) => {
-  console.log(swiper);
+
+// posts => 전체 포스트
+// topRankPosts => 상위 10개 post
+const posts = ref([]);
+const topRankPosts = ref([]);
+const URL = import.meta.env.VITE_BASE_URL;
+const fetchPosts = async () => {
+  try {
+    const response = await axios.get(`${URL}/posts`);
+    // TODO: response.data에서 response.data.posts로 변경
+    posts.value = response.data;
+    // posts.value = response.data.posts;
+    // TODO: response.data에서 topRankPosts로 변경해줘야 함
+    topRankPosts.value = response.data;
+    // topRankPosts.value = response.data.topRankPosts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
 };
-const onSlideChange = () => {
-  console.log('slide change');
-};
+
+onMounted(() => {
+  fetchPosts();
+})
 
 // TODO: board 게시판 정보 불러와야 함
-
-
-
 </script>
 
 <template>
   <div class="board-container">
-    <!--  swiper 부분 (게시글 title, content 적고 넘기는 용도) START -->
+    <!-- swiper 부분 (게시글 title, content 적고 넘기는 용도) START -->
     <div class="swiper-container flex items-center justify-center bg-gray-700 text-black">
       <swiper
           class="h-96"
-          :slides-per-view="5"
+          :slides-per-view="4"
           :space-between="100"
           :breakpoints="{
-            640: { slidesPerView: 1, spaceBetween: 20 },
-            768: { slidesPerView: 2, spaceBetween: 30 },
-            1024: { slidesPerView: 3, spaceBetween: 40 },
-            1280: { slidesPerView: 4, spaceBetween: 50 },
-            1536: { slidesPerView: 5, spaceBetween: 100 }
-          }"
-          @swiper="onSwiper"
-          @slideChange="onSlideChange"
+          640: { slidesPerView: 1, spaceBetween: 20 },
+          768: { slidesPerView: 2, spaceBetween: 30 },
+          1024: { slidesPerView: 3, spaceBetween: 40 },
+          1280: { slidesPerView: 4, spaceBetween: 50 },
+        }"
+
       >
-        <swiper-slide v-for="(i) in 10">
-          <VBoardCard />
+        <swiper-slide v-for="(post) in topRankPosts" :key="post.id">
+          <VBoardCard :post="post" />
         </swiper-slide>
       </swiper>
     </div>
-    <!--  swiper 부분 (게시글 title, content 적고 넘기는 용도) END -->
+    <!-- swiper 부분 (게시글 title, content 적고 넘기는 용도) END -->
 
-    <!--  검색창 START  -->
-    <div>
-      <SearchComponent />
+    <!-- 검색창 START -->
+    <div class="search-container">
+      <SearchComponent :posts="posts" />
     </div>
-    <!--  검색창 END  -->
+    <!-- 검색창 END -->
 
-    <!--  전체 게시글 목록 (제목, 날짜, content:글자수로 자름) | 썸네일 START  -->
-    <div></div>
-    <!--  전체 게시글 목록 (제목, 날짜, content:글자수로 자름) | 썸네일 START  -->
+    <!-- 전체 게시글 목록 (제목, 날짜, content:글자수로 자름) | 썸네일 START -->
+    <BoardContent v-for="(post) in posts" :post="post"/>
+    <!-- 전체 게시글 목록 (제목, 날짜, content:글자수로 자름) | 썸네일 END -->
   </div>
 </template>
 
 <style scoped>
 .board-container {
-  height: 2550px;
+  //height: 2550px;
 }
 .swiper-container {
   height: 25%;
+}
+.search-container {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background-color: white; /* 배경색을 설정하여 스크롤 시 다른 요소와 겹치지 않게 함 */
 }
 </style>
