575a41f1cc02bf88bf05a68e36a68262d6fbc319
고관우 <kwko97@hanyang.ac.kr>
2024-05-23 17:26:52 +0900

    feat: footer 내용 추가

--- enjoytrip_team05/src/views/TheFooterNavbar.vue
+++ enjoytrip_team05/src/views/TheFooterNavbar.vue
@@ -3,11 +3,16 @@
 </script>
 
 <template>
-  <div class="bg-sky-50 h-32">
-
+  <div class="bg-sky-50 h-32 flex justify-center items-center ">
+    <p class="text-color">© CopyRight</p>
+    <p class="text-color">&nbsp;TOT(Take Over Trip)</p>
+    <a href="https://github.com/KwanwooKo" class="text-color">&nbsp;고관우</a>
+    <a href="https://github.com/DDongYul" class="text-color">&nbsp;이동열</a>
   </div>
 </template>
 
 <style scoped>
-
+.text-color {
+  color: #2c3e50; /* 짙은 파란색 */
+}
 </style>
